fd20599769c42b9b091941eeabe7bd0662f5d499
Mark <mark3ting.sci3nc3s@gmail.com>
2017-09-08 09:49:58 +0200

    add clean argument to example.py

--- example.py
+++ example.py
@@ -3,9 +3,13 @@ import wget
 import zipfile
 import pandas as pd
 import os
+import shutil
+import sys
 from pdb import set_trace as bp
 #
 def dclean(dfiles):
+    if os.path.exists('asset'):
+        shutil.rmtree('asset')
     for i, ival in enumerate(dfiles):
         try:
             os.remove(ival)
@@ -17,6 +21,9 @@ zipfilename = 'occupancy_data.zip'
 dfiles = ['datatest2.txt', 'datatest.txt', 'datatraining.txt']
 dfiles.append(zipfilename)
 dclean(dfiles)
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'clean':
+        sys.exit()
 url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/00357/'+zipfilename
 filename = wget.download(url)
 zip_ref = zipfile.ZipFile(filename, 'r')
@@ -29,6 +36,4 @@ if not os.path.exists(directory):
 df['time'] = df.index
 df[['time','Temperature','Humidity']].to_csv(directory+'/sample.csv', index=False)
 import train
-if os.path.exists('asset'):
-        os.remove('asset')
 dclean(dfiles)
